7949e4d066e09eabfe1e3443d22ea87bbc728bcc
BhanupriyaGit <bhanupriyavaddi64@gmail.com>
2025-07-21 15:01:07 -0700

    added index html file

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 print(__name__)
 @app.route("/")
 def hello():
-    return ('<h1 style="text-align:center" >Hello, World!</h1>' 
-            '<p> bhanupriya welcome to flask name main function run in pycharm</p>' 
-            '<img src="https://www.adorama.com/alc/wp-content/uploads/2021/05/bird-wings-flying.gif" width=200px>')
+
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
